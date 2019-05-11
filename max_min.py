6a7fc032fccdc04734ae13f7b3b064544be56abd
Rakesh Kumar Singh <rksingh2004@gmail.com>
2019-05-12 01:52:47 +0530

    max_min.py
    Python - finding max and min values in a list

---
+++ max_min.py
@@ -0,0 +1,29 @@
+# find out the max and min in a list of numbers(unordered)
+list1 = [5,4,1,2,3]
+print (list1)
+
+# using inbuilt function max
+max_a = max(list1)
+print (max_a)
+
+# using inbuilt function min
+min_b = min(list1)
+print (min_b)
+
+# finding max without using in-built function
+max_a1 = 0
+#print (max_a1)
+len1 = len(list1)
+#print (len1)
+i = 0
+for (i; i<n; i++)
+	{
+		if(list1[i] <list1[i+1])
+			{
+				max_a1 = list1[i+1]
+			}
+			else
+				max_a1 = list1[i]
+	}
+print (max_a1)'''
+	
